e4c12cc3e7533c289e93ece727aba9aecfd99fd8
George Salu <george.salu10@gmail.com>
2018-03-17 23:15:24 -0300

    cuidados com inferencias de tipos

---
+++ CursoKotlin/src/fundamentos/CuidadoInferencia1.kt
@@ -0,0 +1,12 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+	//var a , gera erro pos a variavel nao foi inicializada nem tem um tipo
+	var a: Int
+	var b = 2
+	
+	a = 10
+	
+	println(a + b) 
+	 
+}
