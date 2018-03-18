3eb4b7fa1142460d511418c1c70f9f4a208e5cb0
George Salu <george.salu10@gmail.com>
2018-03-17 22:45:25 -0300

    comentarios

---
+++ CursoKotlin/src/fundamentos/Comentarios.kt
@@ -0,0 +1,14 @@
+package fundamentos
+
+/**
+ * Kotlin suporta KDoc... similar a JAVADoc
+ */
+fun main(args: Array<String>) {
+	// Comentario de uma linha
+	println("Opa")
+	
+	/*
+ 		comentario de multiplas linhas
+	*/
+	println("Comantario de mulitplas linhas")
+}
