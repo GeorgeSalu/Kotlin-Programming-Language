7f3be945b4116b9cd09fd30dcea70e97383b6e31
George Salu <george.salu10@gmail.com>
2018-03-19 11:31:21 -0300

    notacao ponto

---
+++ CursoKotlin/src/fundamentos/NotacaoPonto.kt
@@ -0,0 +1,9 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+	val a: Int = 32.dec()
+	val b: String = a.toString()
+	
+	println("Int : $a")
+	println("Primeiro char da string b é : ${b.first()}")
+}
