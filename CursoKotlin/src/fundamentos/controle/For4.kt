7486bd4a616cf105774d5ecdddc343b599684a78
George Salu <george.salu10@gmail.com>
2018-03-19 21:58:05 -0300

    estrutura for

---
+++ CursoKotlin/src/fundamentos/controle/For4.kt
@@ -0,0 +1,10 @@
+package fundamentos.controle
+
+fun main(args: Array<String>) {
+	val alunos = arrayListOf("andre", "carla", "marcos")
+	
+	for((indice, aluno) in alunos.withIndex()) {
+		println("${indice+1} - $aluno")
+	}
+	
+}
