4f3271d1c4033f678a073971511a7bbaa4e16846
George Salu <george.salu10@gmail.com>
2018-03-20 15:59:28 -0300

    checagem de tipos

---
+++ CursoKotlin/src/fundamentos/ChacagemDeTipos.kt
@@ -0,0 +1,12 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+	val valor = "abc"
+	
+	if(valor is String) {
+		println(valor)
+	} else if (valor !is String) {
+		println("nao é uma string")
+	}
+	
+}
