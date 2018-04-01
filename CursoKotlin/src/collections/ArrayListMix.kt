83c1e5cad783c023d949cdd625808149dc24a181
George Salu <george.salu10@gmail.com>
2018-03-31 21:48:29 -0300

    arrayList Heterogenios

---
+++ CursoKotlin/src/collections/ArrayListMix.kt
@@ -0,0 +1,14 @@
+package collections
+
+fun main(args: Array<String>) {
+	val listMix = arrayListOf("Rafael", true, 1, 3.14, 'p')
+	
+	for(item in listMix) {
+		if(item is String) {
+			println(item.toUpperCase())
+		} else {
+			println(item)
+		}
+	}
+	
+}
