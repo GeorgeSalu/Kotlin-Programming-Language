e71589cd3951c513d2c2915705dfe4f56bbbfc5c
George Salu <george.salu10@gmail.com>
2018-03-31 21:34:17 -0300

    usando a funcao arrayListOf

---
+++ CursoKotlin/src/collections/Arrays.kt
@@ -0,0 +1,11 @@
+package collections
+
+fun main(args: Array<String>) {
+	val numeros = Array<Int>(10) { i -> i * 10}
+	
+	for(numero in numeros) {
+		println(numero)
+	}
+	
+	println(numeros.get(1))
+}
