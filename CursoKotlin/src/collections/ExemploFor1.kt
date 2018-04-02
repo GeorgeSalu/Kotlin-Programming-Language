ca6062bfa9d24a5ebd3c46ea09bc6b637f86b5fd
George Salu <george.salu10@gmail.com>
2018-04-01 22:21:56 -0300

    usando for em colecoes

---
+++ CursoKotlin/src/collections/ExemploFor1.kt
@@ -0,0 +1,14 @@
+package collections
+
+fun main(args: Array<String>) {
+	val alunos = arrayListOf("amanda","andre","bernado","carlos")
+	
+	for(aluno in alunos) {
+		println(aluno)
+	}
+	
+	for((indice, aluno) in alunos.withIndex() ) {
+		println("${indice + 1} - $aluno")
+	}
+	
+}
---
+++ CursoKotlin/src/collections/ExemploFor2.kt
@@ -0,0 +1,10 @@
+package collections
+
+fun main(args: Array<String>) {
+	val alunos = arrayListOf("amanda","andre","bernado","carlos")
+	
+	alunos.forEach{
+		println(it)
+	}
+	
+}
