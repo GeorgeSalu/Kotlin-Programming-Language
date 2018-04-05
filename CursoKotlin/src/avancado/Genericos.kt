aa347fc33ddbc7b7dd812246e0150e225e0de198
George Salu <george.salu10@gmail.com>
2018-04-05 16:31:50 -0300

    usando generics

---
+++ CursoKotlin/src/avancado/Genericos.kt
@@ -0,0 +1,23 @@
+package avancado
+
+class Caixa<T>(val objeto: T) {
+	private val objetos = ArrayList<T>()
+	
+	init{
+		adicionar(objeto)
+	}
+	
+	fun adicionar(novoObjeto: T) {
+		objetos.add(novoObjeto)
+	}
+	
+	override fun toString(): String = objetos.toString()
+}
+
+fun main(args: Array<String>) {
+	val materialEscolar = Caixa("Caneta")
+	materialEscolar.adicionar("lapis")
+	materialEscolar.adicionar("Borracha")
+	println(materialEscolar)
+	
+}
